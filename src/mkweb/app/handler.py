735a475f4702bd95b62f910780ff3ad42bcc04ab
jorgechato <hack@jorgechato.com>
2020-08-31 10:56:21 +0200

    UPDATE in-code documentation

--- src/mkweb/app/handler.py
+++ src/mkweb/app/handler.py
@@ -4,6 +4,13 @@ import sys
 
 
 def produce(input, output):
+    """
+    Produce the web from a mkdocs folder
+
+    param: input: mkdocs project path
+    param: output: tmp path for rendered web
+    return: stdout binary
+    """
     # Producing the website
     os.chdir(input)
     os.system(f'mkdocs build -d {output} -q')
@@ -16,6 +23,13 @@ def produce(input, output):
 
 
 def serve(host, port, path):
+    """
+    Serve the documentation page from sdtin binary
+
+    param: host: host of the server, default 0.0.0.0
+    param: port: port of the server, default:8000
+    param: path: tmp path to write the tar file
+    """
     with tarfile.open(fileobj=sys.stdin.buffer, mode='r|gz') as tar:
         tar.extractall(path=path)
 
